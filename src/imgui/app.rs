6043d500c58df06fc695c80fd3912197fe97f8ac
Diego Augusto S. C <diego.augusto@protonmail.com>
2023-04-08 22:10:17 -0300

    Checkpoint commit

--- src/imgui/app.rs
+++ src/imgui/app.rs
@@ -3,16 +3,24 @@ use imgui::Ui;
 use crate::{nodes::{Node, NodeId, Pin, PinId}, app::App};
 
 impl App {
-    pub fn run(&mut self, ui: &Ui, context: &mut imnodes::EditorContext) {
-        imnodes::editor(context, |editor| {
-            for (id, node) in self.nodes.iter() {
-                editor.add_node(imnodes::NodeId(*id), |ui_node| {
-                    for input in node.inputs.iter() {
-                        ui_node.add_input(imnodes::InputPinId(input.id), imnodes::PinShape::Triangle, || {})
+    pub fn draw(&mut self, ui: &Ui, context: &mut imnodes::EditorContext, size: [f32; 2]) {
+        ui.window("the best window ever")
+            .size(size, imgui::Condition::Always)
+            .position([0.0, 0.0], imgui::Condition::Always)
+            .no_decoration()
+            .menu_bar(true)
+            .movable(false)
+            .build(|| {
+                imnodes::editor(context, |editor| {
+                    for (id, node) in self.nodes.iter() {
+                        editor.add_node(imnodes::NodeId(*id), |ui_node| {
+                            for input in node.inputs.iter() {
+                                ui_node.add_input(imnodes::InputPinId(input.id), imnodes::PinShape::Triangle, || {})
+                            }
+                        });
                     }
+                    ;
                 });
-            }
-            ;
-        });
+            });
     }
 }
