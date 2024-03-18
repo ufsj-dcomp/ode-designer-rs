53634c13c461c9ae7a7430fda3b47195fa6ffe9c
Brenno Lemos <brenno@syndel.is>
2024-03-17 21:10:50 -0300

    chore: user string over vec

--- src/core/python.rs
+++ src/core/python.rs
@@ -19,19 +19,19 @@ pub fn execute_python_code(command: &mut Command) -> Result<String, PythonError>
         .spawn()
         .map_err(PythonError::FailedToStartProcess)?;
 
-    let mut output = Vec::new();
+    let mut output = String::new();
     let mut stdout = python_process
         .stdout
         .take()
         .ok_or(PythonError::OutputNotAvailable)?;
 
     stdout
-        .read_to_end(&mut output)
+        .read_to_string(&mut output)
         .map_err(PythonError::FailedToReadOutput)?;
 
     let status = python_process.wait().unwrap();
     if status.success() {
-        Ok(String::from_utf8_lossy(&output).to_string())
+        Ok(output)
     } else {
         Err(PythonError::ProcessFailed(
             status.code().unwrap_or_default(),
